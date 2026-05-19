d5b3927f9779adffe3f3fdf3aff0cf42d33d7f19
Kysohee18 <hanashahky@gmail.com>
2026-05-19 11:10:50 +0700

    feat: update Profile and User entity models

--- main/java/com/example/demo/model/Profile.java
+++ main/java/com/example/demo/model/Profile.java
@@ -1,4 +1,28 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profile")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Profile {
+    @Id
+    private String id;
+    private String nama;
+    private String alamat;
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
 }
