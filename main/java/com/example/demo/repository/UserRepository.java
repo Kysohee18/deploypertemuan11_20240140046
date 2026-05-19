0ce1d1a41418be660effba205523ef496195275c
Kysohee18 <hanashahky@gmail.com>
2026-05-19 10:07:25 +0700

    add repository.java

--- main/java/com/example/demo/repository/UserRepository.java
+++ main/java/com/example/demo/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repository;
 import com.example.demo.model.User;
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername (String  username);
 }
